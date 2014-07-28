8de5ee82e53c1db114c9bb21ffa36e67d41dbb1e
Aaron MacDonald <aaron.macdonald65@yahoo.com>
2014-07-28 20:48:58 +0100

    no message

---
+++ Assets/Script/Aaron_Script.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aaron_Script : MonoBehaviour {
+
+	void Start()
+	{
+		Debug.Log("Mait ill rek ur face on rust mait, your ded kiddo");
+	}
+
+
+
+}
